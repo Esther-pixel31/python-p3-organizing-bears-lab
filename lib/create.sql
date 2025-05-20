9e89d40a1854fe205939bda4b3fe9746f80f3a9e
EstherMutua <esthermutua16@gmail.com>
2025-05-20 15:32:03 +0300

    done this before

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIAMRY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'F', 'Brown', 'Friendly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'M', 'White', 'Friendly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown ', 1, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'Brown', 'Friendly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'F', 'White', 'Friendly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'White', 'Friendly', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'M', 'White', 'Friendly', 1);
